efde0061f0cde27a282ed5191d08e875f43c0842
wahjava <wahjava@@users.noreply.github.com>
2022-12-12 20:20:59 +0000

    nginx_auth_mysql: explicitly set headers_out.www_authenticate->next to NULL
    courtesy: https://hg.nginx.org/nginx/rev/711737177b77
    
    
    git-svn-id: https://code.svn.wordpress.org/nginx_auth_mysql@612 81416fe0-c9fb-472d-a4d9-98af37d240be

--- ngx_http_auth_mysql_module.c
+++ ngx_http_auth_mysql_module.c
@@ -600,6 +600,7 @@ ngx_http_auth_mysql_set_realm(ngx_http_request_t *r, ngx_str_t *realm)
     r->headers_out.www_authenticate->key.len = sizeof("WWW-Authenticate") - 1;
     r->headers_out.www_authenticate->key.data = (u_char *) "WWW-Authenticate";
     r->headers_out.www_authenticate->value = *realm;
+    r->headers_out.www_authenticate->next = NULL;
 
     return NGX_HTTP_UNAUTHORIZED;
 }
